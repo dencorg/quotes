2b04d156cf1a3061cf2c355dc601f5c7102a4718
Dennis Rodis <dencorg@gmail.com>
2019-10-13 22:46:07 +0300

    Change url to quotes

--- quotes/urls.py
+++ quotes/urls.py
@@ -22,5 +22,5 @@ from main import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    path('quote/<int:id>/', views.single_quote, name='single')
+    path('quotes/<int:id>/', views.single_quote, name='single')
 ]
