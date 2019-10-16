ecd9a68a25e0f3a51863662d04922ce7904ee582
Dennis Rodis <dencorg@gmail.com>
2019-10-16 13:47:51 +0300

    Add created_at in model

--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
 class Quote(models.Model):
     text = models.TextField(blank=False, null=False)
     author_name = models.CharField(max_length=255, default='Unknown')
     is_draft = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=now)
+
 
     def __str__(self):
         return self.text
